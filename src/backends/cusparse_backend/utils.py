be9770851517343cd1b3ac50fd2e7f79ca91ac5e
Daria Fomina <dashafomina10102004@gmail.com>
2025-10-22 11:05:12 +0000

    Fix tests and cuda env

--- src/backends/cusparse_backend/utils.py
+++ src/backends/cusparse_backend/utils.py
@@ -1,3 +1,9 @@
+import os
+
+os.environ["CUDA_HOME"] = "/usr/local/cuda"
+os.environ["CUDA_PATH"] = "/usr/local/cuda"
+os.environ["PATH"] = f"/usr/local/cuda/bin:{os.environ['PATH']}"
+
 import glob
 from pathlib import Path
 
@@ -16,10 +22,17 @@ cuda_kernels = load(
     name="cuda_kernels",
     build_directory=str(build_path),
     extra_cflags=["-O3"],
-    extra_cuda_cflags=["-O3", "--use_fast_math", "-arch=sm_80", "--generate-line-info", "-lcusparse"],
-    extra_include_paths=glob.glob(
-        str(repo_root_path / ".venv/lib/python3.11/site-packages/**/include"), recursive=True
-    ),
+    extra_cuda_cflags=[
+        "-O3",
+        "--use_fast_math",
+        "-arch=sm_80",
+        "--generate-line-info",
+        "-lcusparse",
+    ],
+    extra_include_paths=[
+        # *glob.glob(str(repo_root_path / ".venv/lib/python3.11/site-packages/**/include"), recursive=True),
+        "/usr/local/cuda/include"
+    ],
     sources=[path + s for s in sources],
     verbose=True,
 )
